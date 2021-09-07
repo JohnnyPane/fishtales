c87d21c10468bde3b84761d0ed88b63bb24e71b9
Johnny Pane <wabanichi@gmail.com>
2021-09-07 11:38:31 -0500

    Rewrite s3 file

--- client/src/services/s3.js
+++ client/src/services/s3.js
@@ -1,14 +1,13 @@
-import S3FileUpload from 'react-s3'
+import S3FileUpload from "react-s3";
 
 const config = {
   bucketName: "fishermanstales",
   region: "us-east-2",
-  
 };
 
 async function uploadImage(image) {
-  let uploadedImage = await S3FileUpload.uploadFile(image, config)
-  return uploadedImage
-};
+  let uploadedImage = await S3FileUpload.uploadFile(image, config);
+  return uploadedImage;
+}
 
-export { uploadImage }
+export { uploadImage };
