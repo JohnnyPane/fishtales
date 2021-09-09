5e766d4e16edbd4b3882c3cc83eb653b65bc1cab
Johnny Pane <wabanichi@gmail.com>
2021-09-09 16:48:25 -0500

    Correct file path

--- server/app.js
+++ server/app.js
@@ -27,11 +27,11 @@ mongoose
   });
 
   // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, 'client/build')));
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
 
