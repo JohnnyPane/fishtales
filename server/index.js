1fbbe680b4cec662b260c81030d8219673d7b33f
Johnny Pane <wabanichi@gmail.com>
2021-09-09 15:37:16 -0500

    require express

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const app = require("./app"); // the actual Express application
 const http = require("http");
 const config = require("./utils/config");
 const logger = require("./utils/logger");
+const express = require("express");
 
 const server = http.createServer(app);
 
