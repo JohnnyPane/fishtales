bec9125c365276515aff5f6d85ecc9d4176376af
Johnny Pane <wabanichi@gmail.com>
2021-09-09 13:20:17 -0500

    Style Stats header

--- client/src/components/FishStats.js
+++ client/src/components/FishStats.js
@@ -11,11 +11,11 @@ const useStyles = makeStyles((theme) => ({
   root: {
     width: "100%",
     alignItems: "center",
-    justifyContent: "space-evenly"
+    justifyContent: "space-evenly",
   },
   heading: {
     fontSize: theme.typography.pxToRem(15),
-    flexBasis: "33.33%",
+    flexBasis:  "100%",
     flexShrink: 0,
   },
   secondaryHeading: {
