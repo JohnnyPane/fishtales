32889dabb9563bc3850e4d0cba6c3d6d12d3c37c
Johnny Pane <wabanichi@gmail.com>
2021-09-09 15:32:35 -0500

    Get ready for deploy

--- server/index.js
+++ server/index.js
@@ -5,6 +5,15 @@ const logger = require("./utils/logger");
 
 const server = http.createServer(app);
 
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
+
 server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
 });
